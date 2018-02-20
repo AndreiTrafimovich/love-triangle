43910edbdbca5f85cb2bc633872fd01af0bc38f0
atr <atr@ciklum.com>
2018-02-20 12:58:49 +0300

    Task triangle

--- src/index.js
+++ src/index.js
@@ -3,5 +3,17 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+    var arr = [];
+    var count = 0;
+    for (var i=0; i<preferences.length; i++) {
+        var first  = preferences[i]-1;
+        var second = preferences[first]-1;
+        var third  = preferences[second]-1;
+        if (third == i && arr.indexOf(third) == -1 && first != second) {
+            arr.push.apply(arr, [first, second, third]);
+            count++;
+        }
+    }
+
+    return count;
 };
